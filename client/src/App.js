174b3e8dcc2e4afd35648c84b0442b738d71165e
govgovnahr <govindn3@illinois.edu>
2022-01-25 16:56:59 -0600

    Added elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Jobs from './Jobs';
+
+const mockJobs = [
+  {title: 'JR SWE', company: 'Amazon'},
+  {title: 'Job1', company: 'Company1'},
+  {title: 'Job2', company: 'Company2'},
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jobs jobs={mockJobs}/>
     </div>
   );
 }
---
+++ client/src/Job.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+
+export default function Job( {job}) {
+    return (
+        <div className={'job'}>
+            <div>{job.title}</div> 
+             <div>{job.company}</div>
+        </div>
+    )
+}
