807531cbf99be669b74ac11e4724761e550f33ed
govgovnahr <govindn3@illinois.edu>
2022-01-26 13:38:59 -0600

    starting backend

---
+++ worker/tasks/fetch-github.js
@@ -0,0 +1,22 @@
+var fetch = require('node-fetch');
+
+
+const baseURL = 'string';
+async function fetchGithub() {
+    let resultCount= 1, onPage = 0;
+    const allJobs = [];
+
+    while(resultCount > 0) {
+        const res = await fetch(`${baseURL}?page=${onPage}`);
+        const jobs = await res.json();
+        allJobs.push(...jobs);
+        resultCount = jobs.length
+        console.log('got', jobs.length, 'jobs');
+        onPage++;
+    }
+    console.log('got', allJobs.length, 'jobs')
+}
+
+fetchGithub();
+
+module.exports = fetchGithub;
