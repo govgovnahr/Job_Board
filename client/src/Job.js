004b65cd6fa7c101a3e863295fd2d6de68a0d437
govgovnahr <govindn3@illinois.edu>
2022-01-26 22:21:23 -0600

    Remove deprecated API

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 
 export default function Job( {job}) {
+
     return (
         <div className={'job'}>
             <div>{job.title}</div> 
--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,22 +1,38 @@
-var fetch = require('node-fetch');
+const axios = require('axios');
 
+// const baseURL = 'string';
+// async function fetchGithub() {
+//     let resultCount= 1, onPage = 0;
+//     const allJobs = [];
 
-const baseURL = 'string';
-async function fetchGithub() {
-    let resultCount= 1, onPage = 0;
-    const allJobs = [];
+//     while(resultCount > 0) {
+//         const res = await fetch(`${baseURL}?page=${onPage}`);
+//         const jobs = await res.json();
+//         allJobs.push(...jobs);
+//         resultCount = jobs.length
+//         console.log('got', jobs.length, 'jobs');
+//         onPage++;
+//     }
+//     console.log('got', allJobs.length, 'jobs')
+// }
 
-    while(resultCount > 0) {
-        const res = await fetch(`${baseURL}?page=${onPage}`);
-        const jobs = await res.json();
-        allJobs.push(...jobs);
-        resultCount = jobs.length
-        console.log('got', jobs.length, 'jobs');
-        onPage++;
+async function getJobs() {
+    try {
+        const jobs = await axios.get("https://api.lever.co/v0/postings/lever?mode=json&limit=10");
+        let jobNames = [];
+        for (let i = 0; i < jobs["data"].length; i++) {
+            jobNames.push(jobs["data"][i]["text"]);
+        }
+        console.log(jobNames);
+        return jobNames;
+    } catch(error) {
+        console.log("There was an error: ", error);
+        return [];
     }
-    console.log('got', allJobs.length, 'jobs')
 }
 
-fetchGithub();
 
-module.exports = fetchGithub;
+
+getJobs();
+
+module.exports = getJobs;
